a0c04b5da7fc3df755abd4bdc4be7570ff66ce2b
Wolf-3317 <gankokcheng7@gmail.com>
2026-06-23 12:55:24 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "../../memes/Components/Header"
+import Main from "../../memes/Components/Main"
+import './index.css'
+
+
+export default function App() {
+  return (
+    <>
+    <Header />
+    <Main />
+    </>
+  )
+} 
